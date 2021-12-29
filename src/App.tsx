3b986b9e011b534a26efde242107d72c6e1cd6b2
MikeNotu <mikebateman98@gmail.com>
2021-12-29 13:35:50 -0600

    TS practice 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { TextField } from "./TextField";
+import React, {useState} from 'react'
 
-const App: React.FC = () =>{
-  return <div>
-<TextField text="Test" person={{firstName:'',lastName:''}} />
-  </div>
+ function App() {
+
+  const [number,setNumber] = useState<number|string>(5);
+
+  const changeNumber = () => {
+    setNumber(10);
+  }
+
+
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
 }
 
 export default App;
