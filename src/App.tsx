a37b26d97c66f73889f1e89aa467422eb8d7a7d2
MikeNotu <mikebateman98@gmail.com>
2021-12-29 13:52:54 -0600

    TS practice 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,33 @@ import React, {useState} from 'react'
 
  function App() {
 
-  const [number,setNumber] = useState<number|string>(5);
+  const [people,setPeople] = useState([
+    {
+    name: "LeBron James",
+    url: "",
+    age: 36,
+    note: "Allergic to staying on the same team"
+  },
+  {
+    name: "Kobe Bryant",
+    url: "",
+    age: 36
+  },
+])
 
-  const changeNumber = () => {
-    setNumber(10);
-  }
+const element = people.map(jugador=>{
+  return(
+  <p>{jugador.name}</p>
+)})
 
 
   return (
-    <div>
-      <h1>Test</h1>
+    <div className='App'>
+      <h1>People Invited to my Party</h1>
+      {element}
+      <h3>hola</h3>
+      <button></button>
+
     </div>
   )
 }
